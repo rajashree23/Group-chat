65628b407de2acc4c0312051a6aa000fa79ed8cf
mishramonalisha76 <mishramonalisha76@gmail.com>
2019-04-14 23:41:08 +0530

    added function for leave operation

--- controller/admin.js
+++ controller/admin.js
@@ -1,17 +1,20 @@
 var mongoose = require('mongoose');
 var adminModel = require('../models/admin');
 
-var admin = function(req,res){
+var adminj = function(req,res){
   req.session.topic = req.params.id;
-  
-  adminModel.findOne({topic: { $elemMatch: { topicname: { req.session.topic} , memberemail: {req.session.email} } },function(err,users){
+  var ctopic=req.session.topic;
+  var member=req.session.email;
+  adminModel.findOne({ "topic.topicname":ctopic},{"topic.$":1} ,function(err,users){
+    console.log("hii",users.memberemail);
+
     if(users){
 
-       return res.redirect('topic/'+req.session.topic);
+       return res.redirect('/topic/'+req.session.topic);
 
     }
     else {
-      adminModel.updateOne({"topic.topicname":   req.session.topic },
+      adminModel.updateOne({'topic.topicname':  ctopic },
        {$push:{topic:[{
          topicname:{},
          memberemail:[],
@@ -20,12 +23,31 @@ var admin = function(req,res){
        function(err,raw){
            console.log(raw);
        });
+
           return res.redirect('/profile');
     }
 
-  });
+
 });
 }
+var adminl=function(req,res){
+  req.session.topic = req.params.id;
+  var ctopic=req.session.topic;
+  var member=req.session.email;
+  adminModel.updateOne({'topic.topicname':  ctopic },
+   {$pull:{topic:[{
+     topicname:{},
+     memberemail:[req.session.user.email],
+     request:[]
+   }]}},
+   function(err,raw){
+       console.log(raw);
+   });
+     return res.redirect('/profile');
+}
+
 module.exports = {
-    "admin": admin,
+    "adminj": adminj,
+    "adminl":adminl,
+
 };
--- routes/index.js
+++ routes/index.js
@@ -80,8 +80,8 @@ router.post('/create', create.topics);
 router.get('/display', topics.display);
 router.post('/send', send.message);
 router.post('/refresh', refresh.messages);
-router.get('/topicj/:id', admin.admin);
-router.get('/topicl/:id', admin.admin);
+router.get('/topicj/:id', admin.adminj);
+router.get('/topicl/:id', admin.adminl);
 
 router.get('/logout', function(req, res) {
     req.session.user = null;
